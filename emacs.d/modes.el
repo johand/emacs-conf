2cba80252eaa678098ce0a90e98dfad36fc9d20b
Johan <johand@openmailbox.org>
2017-01-03 21:36:30 -0400

    Updating smartparens config
    * Indent for pairs
    
    * Turn on strict mode

--- emacs.d/modes.el
+++ emacs.d/modes.el
@@ -125,10 +125,23 @@
 
 ;;; smartparens
 
-(use-package smartparens
+(use-package smartparens-config
+  :ensure smarparens
   :config
-  (smartparens-global-mode t))
+  (progn
+    (show-smartparens-global-mode t)))
 
+(sp-with-modes '(ruby-mode js-mode web-mode)
+  (sp-local-pair "{" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair "[" nil :post-handlers '((my-create-newline-and-enter-sexp "RET")))
+  (sp-local-pair "(" nil :post-handlers '((my-create-newline-and-enter-sexp "RET"))))
+
+(defun my-create-newline-and-enter-sexp (&rest _ignored)
+  "Open a new brace or bracket expression, with relevant newlines and indent. "
+  (newline)
+  (indent-according-to-mode)
+  (forward-line -1)
+  (indent-according-to-mode))
 
 ;;; smex
 
